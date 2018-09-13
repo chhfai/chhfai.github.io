4377de8e81440dd1327351384b011a83cd1d3ad3
Heng Fai Chang, Jason <chhfai@gmail.com>
2018-09-13 11:55:10 +0800

    HTML,CSS,Javascript update: Created the basic framework so that the website can be put in use

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('script.js is working')
+nav = document.querySelector(".nav")
+
+async function getTextFile(file) {
+    try {    
+        const response = await fetch(file);
+        const text = await response.text();
+        return text;
+    } catch(err) {
+        console.log(err);
+    }
+}
+async function wrapper() {
+    navHTML= await getTextFile("./nav.html");
+    nav.innerHTML = navHTML
+}
+
+wrapper()
